4094c1ebec37d01df72bc4c9228aae960d771c49
pronix <pronix.service@gmail.com>
2011-01-11 12:25:35 +0500

    changes rails version to 3.0.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppTest::Application.config.secret_token = '72170275be680bf9387ee5e97c073b998b7050374726265d28efdfff0191e5e0d77ed37178de2d81953fdf799647f9939d8cdadded82e0d739ed99cae41bf24f'
