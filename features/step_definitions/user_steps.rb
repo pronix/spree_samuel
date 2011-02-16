68c3385618d1887481c8b3ad1f0d6a2111dd9fc7
pronix <pronix.service@gmail.com>
2011-02-16 22:38:10 +0500

    copy standart steps from spree

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,3 +1,8 @@
-Given /^I am not logged in$/ do
-  visit "/logout"
+Given /^no user exists with an email of "(.*)"$/ do |email|
+  User.find_by_email(email).should be_nil
 end
+
+Then /^(\d+) users should exist$/ do |c|
+  User.count.should == c.to_i
+end
+
