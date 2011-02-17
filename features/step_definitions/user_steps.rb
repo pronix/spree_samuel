ae6af27e8ebfdebf3e284960f58871a3d0d2112e
pronix <pronix.service@gmail.com>
2011-02-17 11:29:39 +0500

    added user factory

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -6,3 +6,7 @@ Then /^(\d+) users should exist$/ do |c|
   User.count.should == c.to_i
 end
 
+Then /^I should see login form$|^I am shown the user login page$/ do
+  page.should have_css("form[action='/users/sign_in']")
+end
+
