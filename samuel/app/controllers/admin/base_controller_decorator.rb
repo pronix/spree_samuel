e6a2ab106335b9de5ddc3fe13f456f95a88c2500
pronix <pronix.service@gmail.com>
2011-02-18 19:10:03 +0500

    поправил выбор слоя для продавцов

--- samuel/app/controllers/admin/base_controller_decorator.rb
+++ samuel/app/controllers/admin/base_controller_decorator.rb
@@ -2,7 +2,8 @@ Admin::BaseController.class_eval do
   layout :detect_layout
 
   def detect_layout
-    (current_user && current_user.has_role?(:seller)) ? "seller" : "admin"
+    (current_user &&
+     current_user.has_role?(:seller) && !current_user.has_role?(:admin)) ? "seller" : "admin"
   end
 end
 
