151617eabf9113bbe1d179d74f52f33e9388e270
pronix <pronix.service@gmail.com>
2011-02-19 21:29:20 +0500

    фичи, просмотр таксонов продавцом

--- features/step_definitions/products.rb
+++ features/step_definitions/products.rb
@@ -16,19 +16,3 @@ Given /^the following products exist:$/ do |table|
 
   end
 end
-
-
-Given /^the following categories exist:$/ do |table|
-  data = table.raw
-  taxonomy_category = Factory.create(:taxonomy,{ :name => "Categories"})
-  taxon_category = Taxon.find_by_name("Categories") ||
-    Factory.create(:root_taxon, { :name =>"Categories", :taxonomy =>taxonomy_category } )
-
-  data.each do |taxons|
-    @root_taxon = taxon_category
-    taxons.each do |taxon|
-      @root_taxon = Taxon.find_by_name(taxon) ||
-        Factory.create(:taxon,{ :parent => @root_taxon, :name => taxon, :taxonomy =>taxonomy_category })
-    end
-  end
-end
