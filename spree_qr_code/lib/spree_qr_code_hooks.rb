fb065eb544491213a25634f984ba3f21887425d7
pronix <pronix.service@gmail.com>
2011-02-23 00:27:50 +0500

    2057: Added BIG_SIZE

--- spree_qr_code/lib/spree_qr_code_hooks.rb
+++ spree_qr_code/lib/spree_qr_code_hooks.rb
@@ -11,7 +11,7 @@ class SpreeQrCodeHooks < Spree::ThemeSupport::HookListener
   insert_after :order_details_line_item_row do
       %(
           <% if item.qr_code %>
-            <td class="qr_code"><%= link_to (image_tag qr_code_path(:id => item.qr_code.code)),  qr_code_path(:id => item.qr_code.code, :size => 16) %></td>
+            <td class="qr_code"><%= link_to (image_tag qr_code_path(:id => item.qr_code.code)),  qr_code_path(:id => item.qr_code.code, :size => QrCode::BIG_SIZE) %></td>
             <td class="qr_code"><%= item.qr_code.count - item.qr_code.used_count %></td>
           <% end %>
        )
